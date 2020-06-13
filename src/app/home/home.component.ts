1c2a2e40d22ba4f2dddea2ace26529946da0a69e
codeconcept <contact@codeconcept.fr>
2020-06-13 18:42:52 +0200

    Sweet Alert with input text

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,10 +6,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  message;
+  favoriteTVshow = '';
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  saveTVshow(tvShow) {
+    console.log(tvShow);
+    this.favoriteTVshow = tvShow;
+  }
+
+  cancelTVshowAlert(e) {
+    console.log(e); 
+    switch (e) {
+      case 'esc':
+        this.message = 'Echap bien pris en compte'
+        this.hideMessage(1500);
+        break;
+      case 'backdrop':
+          this.message = `Vous avez cliqué sur l'arrière-plan`;
+          this.hideMessage(1500);
+          break;
+      default:
+        this.message = 'bye'
+        break;
+    }   
+  }
+
+  hideMessage(ms: number) {
+    setTimeout(() => this.message = '', ms);
   }
 
 }
